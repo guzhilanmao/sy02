439e5b544d1a3e6924b32a5907720b910933d82c
lanmao <3452388269@qq.com>
2022-07-13 11:19:26 +0800

    彭佳威是个大傻逼

---
+++ src/main/java/wdasdaw.java
@@ -0,0 +1,12 @@
+/**
+ * @program: GitTest
+ * @description: ww
+ * @author: Mr.Wang
+ * @create: 2022-07-13 11:18
+ **/
+public class wdasdaw {
+
+    public static void main(String[] args) {
+        System.out.println("彭佳威是个大傻逼");
+    }
+}
